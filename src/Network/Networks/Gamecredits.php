ea11fbda98f9401e420e84d213de24bf9b5ec023
Gametoshi <44985491+Globaleliteee@users.noreply.github.com>
2018-11-16 23:49:48 +0100

    Create Gamecredits.php for bitcoin-php

---
+++ src/Network/Networks/Gamecredits.php
@@ -0,0 +1,49 @@
+<?php
+namespace BitWasp\Bitcoin\Network\Networks;
+use BitWasp\Bitcoin\Network\Network;
+use BitWasp\Bitcoin\Script\ScriptType;
+class Gamecredits extends Network
+{
+    /**
+     * {@inheritdoc}
+     * @see Network::$base58PrefixMap
+     */
+    protected $base58PrefixMap = [
+        self::BASE58_ADDRESS_P2PKH => "38",
+        self::BASE58_ADDRESS_P2SH => "62",
+        self::BASE58_WIF => "1a",
+    ];
+    /**
+     * {@inheritdoc}
+     * @see Network::$bech32PrefixMap
+     */
+    protected $bech32PrefixMap = [
+        self::BECH32_PREFIX_SEGWIT => "game",
+    ];
+    /**
+     * {@inheritdoc}
+     * @see Network::$bip32PrefixMap
+     */
+    protected $bip32PrefixMap = [
+        self::BIP32_PREFIX_XPUB => "019da462",
+        self::BIP32_PREFIX_XPRV => "019d9cfe",
+    ];
+    /**
+     * {@inheritdoc}
+     * @see Network::$bip32ScriptTypeMap
+     */
+    protected $bip32ScriptTypeMap = [
+        self::BIP32_PREFIX_XPUB => ScriptType::P2PKH,
+        self::BIP32_PREFIX_XPRV => ScriptType::P2PKH,
+    ];
+    /**
+     * {@inheritdoc}
+     * @see Network::$signedMessagePrefix
+     */
+    protected $signedMessagePrefix = "Gamecredits Signed Message";
+    /**
+     * {@inheritdoc}
+     * @see Network::$p2pMagic
+     */
+    protected $p2pMagic = "dbb6c0fb";
+}
